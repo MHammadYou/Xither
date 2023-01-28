14c53e385c0b36d3dedd955e3d4843d3ffc4df91
MHammadYou <MHammadY2.0@gmail.com>
2023-01-28 22:28:55 +0500

    Added TokenType enum

---
+++ src/lexer/token.rs
@@ -0,0 +1,34 @@
+
+
+#[derive(Debug)]
+enum TokenType {
+    // Grammar
+    LeftParen, RightParen,
+    LeftCurly, RightCurly,
+    LeftSqure, RightSqure,
+    SemiColon, Comma, Dot, DotDot,
+
+    // Operators
+    Plus, Minus,
+    Star, Slash,
+    Less, LessEqual,
+    Greater, GreaterEqual,
+    Equal, EqualEqual,
+    Bang, BangEqual,
+    PlusEqual, MinusEqual,
+
+    // Literals
+    Identifier, String, Number,
+    None, True, False,
+
+    // Reserved keywords
+    If, Else, Loop, Let, Const,
+    Fn, Class, Return, At, SelfTok,
+    And, Or, Break, Continue,
+
+    // Built-ins
+    LogFn, TypeFn, NumberFn, 
+    StringFn, BooleanFn,
+
+    EOF
+}
