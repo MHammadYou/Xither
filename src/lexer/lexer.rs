f5d63e6dec9e602182f98fc3e05e23914205eda2
MHammadYou <MHammadY2.0@gmail.com>
2023-01-31 23:16:22 +0500

    Added fn get_tokens()

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use super::token::TokenType;
+use super::token::{TokenType, Token};
 
 
 pub struct Lexer<'a> {
@@ -20,5 +20,11 @@ impl<'a> Lexer<'a> {
             line: 1,
             keywords: HashMap::new()
         }
+    }
+
+    pub fn get_tokens(&mut self) -> Vec<Token> {
+        let mut tokens = vec![];
+
+        tokens
     }
 }
