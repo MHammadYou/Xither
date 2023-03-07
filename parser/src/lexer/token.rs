21c92f6a3c612d70908b34b914d8b426d65c58a4
MHammadYou <MHammadY2.0@gmail.com>
2023-03-07 16:34:03 +0500

    Added Cli binary crate. Removed global Cargo.toml

---
+++ parser/src/lexer/token.rs
@@ -0,0 +1,51 @@
+
+
+#[derive(Debug)]
+pub enum TokenType {
+    // Grammar
+    LeftParen, RightParen,
+    LeftCurly, RightCurly,
+    LeftSqure, RightSqure, At,
+    SemiColon, Comma, Dot, DotDot,
+
+    // Operators
+    Plus, Minus,
+    Star, Slash,
+    Less, LessEqual,
+    Greater, GreaterEqual,
+    Equal, EqualEqual,
+    Bang, BangEqual,
+    PlusEqual, MinusEqual,
+
+    // Literals
+    Identifier, String, Number,
+    None, True, False,
+
+    // Reserved keywords
+    If, Else, Loop, Let, Const,
+    Fn, Class, Return, SelfTok,
+    And, Or, Break, Continue,
+
+    // Built-ins
+    LogFn, TypeFn, NumFn, 
+    StrFn, BoolFn,
+
+    EOF
+}
+
+#[derive(Debug)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub lexeme: String,
+    pub line: u32
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, line: u32) -> Token {
+        Token {
+            token_type,
+            lexeme,
+            line
+        }
+    }
+}
