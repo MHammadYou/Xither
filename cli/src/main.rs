52635a4a8d69ac2f2aa408a389c52e84a973a36b
MHammadYou <MHammadY2.0@gmail.com>
2023-11-22 22:17:48 +0500

    Update cli to accept filename args to parse source

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,21 @@
+use std::{ fs, env };
+
+use parser::Lexer;
+
 
 fn main() {
-    println!("Hello, World!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 {
+        let filename = &args[1];
+        let source = fs::read_to_string(filename);
+
+        if let Ok(source) = source {
+            let tokens = Lexer::new(&source).get_tokens();
+            println!("{:#?}", tokens);
+        }
+    } else {
+        eprintln!("Error: No filename provided")
+    }
+
 }
